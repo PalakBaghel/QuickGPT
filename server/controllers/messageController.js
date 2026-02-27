d097006187c6b2d7cc21866fe7cad8fd8d8b81db
Palak Baghel <palakbaghel09@gmail.com>
2026-02-27 17:31:26 -0500

    quickgpt

---
+++ server/controllers/messageController.js
@@ -0,0 +1,103 @@
+import axios from "axios";
+import Chat from "../models/Chat.js";
+import openai from "../configs/openai.js";
+import imagekit from "../configs/imageKit.js";
+
+
+//Text based AI Chat Message Controller
+export const textMessageController = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        //check credits
+        if (req.user.credits < 1) {
+            return res.json({ success: false, message: "You don't have enough credits to use this feature" })
+        }
+
+        const { chatId, prompt } = req.body
+
+        const chat = await Chat.findOne({ userId, _id: chatId })
+        chat.messages.push({ role: "user", content: prompt, timeStamp: Date.now(), isImage: false })
+
+        const { choices } = await openai.chat.completions.create({
+            model: "gemini-3-flash-preview",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+        });
+
+        const reply = { ...choices[0].message, timeStamp: Date.now(), isImage: false }
+
+
+        chat.messages.push(reply)
+        await chat.save();
+
+        await User.updateOne({ _id: userId }, { $inc: { credits: -1 } })
+
+        res.json({ succes: true, reply })
+
+    } catch (error) {
+        return res.json({ success: false, message: error.message })
+    }
+}
+
+//Image generation Message controller
+export const imageMessageController = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        //check credits
+        if (req.user.credits < 2) {
+            return res.json({ success: false, message: "You don't have enough credits to use this feature" })
+        }
+
+        const { prompt, chatId, isPublished } = req.body
+
+        //Find chat
+        const chat = await Chat.findOne({ userId, _id: chatId })
+
+        //Push user message
+        chat.messages.push({ role: "user", content: prompt, timeStamp: Date.now(), isImage: false })
+
+        //Encode the prompt
+        const encodedPrompt = encodeURIComponent(prompt)
+
+        //construct ImageKit AI generation URL
+        const generatedImageUrl = `${process.env.IMAGEKIT_URL_ENDPOINT}/ik-genimg-prompt-${encodedPrompt}/quickgpt/${Date.now()}.png?tr=w-800,h-800`;
+
+        //Trigger generation by fetching from ImageKit
+        const aiImageResponse = await axios.get(generatedImageUrl, { responseType: "arraybuffer" })
+
+        //convert to Base64
+        const base64Image = `data:image/png;base64,${Buffer.from(aiImageResponse.data, "binary").toString('base64')}`
+
+        //upload to imagekit Media Library
+        const uploadResponse = await imagekit.upload({
+            file: base64Image,
+            fileName: `${Date.now()}.png`,
+            folder: "quickgpt"
+        })
+
+        const reply = {
+            role: 'assistant',
+            content: uploadResponse.url,
+            timeStamp: Date.now(),
+            isImage: true,
+            isPublished
+        }
+
+
+        chat.messages.push(reply)
+        await chat.save();
+
+        await User.updateOne({ _id: userId }, { $inc: { credits: -2 } })
+
+        res.json({ success: true, reply })
+
+    } catch (error) {
+        return res.json({ success: false, message: error.message })
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import connectDB from './configs/db.js'
+import userRouter from './routes/userRoutes.js'
+import chatRouter from './routes/chatRoutes.js'
+import messageRouter from './routes/messageRoutes.js'
+import creditRouter from './routes/creditRoutes.js'
+import { Webhooks } from 'openai/resources/webhooks.mjs'
+import { StripeWebhooks } from './controllers/webhooks.js'
+
+const app = express()
+
+//connection database
+await connectDB()
+
+//Stripe Webhooks
+app.post('/api/stripe', express.raw({type: 'application/json'}), StripeWebhooks)
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+//routes
+app.get('/', (req, res)=> res.send('Server is Live!!'))
+app.use('/api/user', userRouter)
+app.use('/api/chat', chatRouter)
+app.use('/api/message', messageRouter)
+app.use('/api/credit', creditRouter)
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`); 
+})
